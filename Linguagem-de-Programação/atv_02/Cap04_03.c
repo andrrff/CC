e74765eae0b66c49e9f2f6b278f8760bd37c7b9d
André da Silva Freitas <andrecastanhal@Gmail.com>
2021-03-03 16:49:02 -0300

    Nota de corte

---
+++ Linguagem-de-Programação/atv_02/Cap04_03.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+ 
+int main()
+{
+    //! Nota de corte
+    printf("Insira 10 numeros:\n>> ");
+    int numeros[10];
+    scanf("%i", &numeros[0]);
+    scanf("%i", &numeros[1]);
+    scanf("%i", &numeros[2]);
+    scanf("%i", &numeros[3]);
+    scanf("%i", &numeros[4]);
+    scanf("%i", &numeros[5]);
+    scanf("%i", &numeros[6]);
+    scanf("%i", &numeros[7]);
+    scanf("%i", &numeros[8]);
+    scanf("%i", &numeros[9]);
+    printf("Media = %i", (numeros[0] + numeros[1] + numeros[2] + 
+                         numeros[3] + numeros[4] + numeros[5] + 
+                         numeros[6] + numeros[7] + numeros[8] + 
+                         numeros[9]) / (sizeof(numeros) / sizeof(numeros[0])) );
+    if ((numeros[0] + numeros[1] + numeros[2] + 
+                         numeros[3] + numeros[4] + numeros[5] + 
+                         numeros[6] + numeros[7] + numeros[8] + 
+                         numeros[9]) / (sizeof(numeros) / sizeof(numeros[0])) >  5)
+        printf("Passou!");
+    else
+        printf("Reprovou");
+    return 0;
+}
